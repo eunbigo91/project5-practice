093242744c9536228bcedfa1343debe66eb28902
Eunbi Go <eunbigo91@gmail.com>
2016-11-15 17:53:04 -0800

    Style: arrange orders

--- database_test.py
+++ database_test.py
@@ -2,15 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
-
-# Create
-
 engine = create_engine('sqlite:///restaurantmenu.db')
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
+
+# Create
 myFirstRestaurant = Restaurant(name = "Pizza Palace")
 session.add(myFirstRestaurant)
 session.commit()
