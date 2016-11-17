32985a282829ff3100571be592cc41f8ecca23cc
Eunbi Go <eunbigo91@gmail.com>
2016-11-16 17:23:58 -0800

    Add: codes for using database and read restaurant list
    Removed some useless codes related /hola.

--- webserver.py
+++ webserver.py
@@ -1,31 +1,41 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
-class WebServerHandler(BaseHTTPRequestHandler):
+## import CRUD Operations
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 
+
+# Create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+
+class WebServerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
+            if self.path.endswith("/restaurant"):
+                result = session.query(Restaurant).all()
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                for restaurant in result:
+                    output += restaurant.name
+                    output += "</br>"
+                self.wfile.write(output)
+                return
+
             if self.path.endswith("/hello"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output = ""
                 output += "<html><body>Hello!"
-                output += "<form method='POST' enctype='multipart/form-data' action='/hello'>"
-                output += "<h2> What would you like me to say? </h2>"
-                output += "<input name='message' type='text'>"
-                output += "<input type='submit' value='Submit'></form>"
-                output += "</body></html>"
-
-                self.wfile.write(output)
-                print output
-                return
-            if self.path.endswith("/hola"):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                output = ""
-                output += "<html><body>&#161Hola! <a href='/hello'>Back to Hello</a></body></html>"
                 output += "<form method='POST' enctype='multipart/form-data' action='/hello'>"
                 output += "<h2> What would you like me to say? </h2>"
                 output += "<input name='message' type='text'>"
